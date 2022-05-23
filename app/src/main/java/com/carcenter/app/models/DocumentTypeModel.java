d8348ad4fddc41240674fecebead58c9f5117b93
Jamara1 <7.6amarajohan@gmail.com>
2022-05-22 19:35:51 -0500

    Developing:
    - Template created to modularize views
    - The ui composition is implemented in the views for modularization
    - Document type and client models added
    - Document type and client controllers added
    - Faces config added new routes

---
+++ app/src/main/java/com/carcenter/app/models/DocumentTypeModel.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.carcenter.app.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jamar
+ */
+public class DocumentTypeModel {
+    
+    private int id;
+    private String typeName;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+}
